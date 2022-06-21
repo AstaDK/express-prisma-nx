2a79a2257b850ed2245588d0663fac98b06c5b6a
AstaDk <asta.dev.520@gmail.com>
2022-06-21 21:37:36 +0700

    [Demo] use pisma client at react

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,51 +1,66 @@
-
-import styled from 'styled-components';
+import styled from "styled-components";
 import NxWelcome from "./nx-welcome";
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from "react-router-dom";
+import { useEffect } from "react";
+// import { PrismaClient } from "@prisma/client";
 
 const StyledApp = styled.div`
-    // Your style here
+  // Your style here
 `;
 
-
 export function App() {
+  useEffect(() => {
+    (async () => {
+      //! Can not get prismaClient on browser
+      // const prisma = new PrismaClient();
+      // const users = await prisma.user.findMany();
+      // console.log(users);
+    })();
+  }, []);
 
   return (
-      <StyledApp>
-        <NxWelcome title="dashboard"/>
-      
-    {/* START: routes */}
-    {/* These routes and navigation have been generated for you */}
-    {/* Feel free to move and update them to fit your needs */}
-    <br/>
-    <hr/>
-    <br/>
-    <div role="navigation">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/page-2">Page 2</Link></li>
-      </ul>
-    </div>
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div>This is the generated root route. <Link to="/page-2">Click here for page 2.</Link></div>
-        }
-      />
-      <Route
-        path="/page-2"
-        element={
-          <div><Link to="/">Click here to go back to root page.</Link></div>
-        }
-      />
-    </Routes>
-    {/* END: routes */}
+    <StyledApp>
+      <NxWelcome title="dashboard" />
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{" "}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
     </StyledApp>
   );
-
 }
-
 
 export default App;
